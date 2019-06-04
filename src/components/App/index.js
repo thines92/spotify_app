a846897ea7b0793209f8e114bdae4ff044fe2646
Tyler <tylerhines1992@gmail.com>
2019-06-04 19:20:02 -0400

    added components/App

---
+++ src/components/App/index.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+// import *filename* from '../foldername'
+
+class App extends Component {
+  render() {
+    return (
+      <React.Fragment>
+        {/* <Component /> */}
+        <main>
+            {/* <Component /> */}
+        </main>
+        {/* <Component /> */}
+      </React.Fragment>
+    );
+  }
+}
+
+export default App;
