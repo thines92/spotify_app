ef3214bb92af4d23de07ee92bdbd4f17d4cd02c4
Tyler <tylerhines1992@gmail.com>
2019-06-04 20:10:24 -0400

    added Login/index.js

---
+++ src/components/Login/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class Login extends Component {
+
+
+    render() {
+        return (
+            <div>
+                <button>Login</button>
+            </div>
+        )
+    }
+}
+
+export default Login;
